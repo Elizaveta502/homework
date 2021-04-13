adab44250527f2e323e57898a83f200c8613aaa4
Elizaveta502 <82108873+Elizaveta502@users.noreply.github.com>
2021-04-13 15:03:24 +0300

    Add files via upload

---
+++ классы..cpp
@@ -0,0 +1,167 @@
+/* Класс «Рациональное число» (РЧ). Поля: числитель, знаменатель. Методы: перегрузка
+операции приведения типа к float, операций умножения «*» двух РЧ и РЧ с целым числом,
+сравнения на равенство «==» двух РЧ и РЧ с целым числом. */
+
+#include <stdio.h> //подключение библиотек
+#include <iostream>
+#include <locale.h>
+
+using namespace std; //пространство имен
+
+class RNum //определение класса
+{
+	private: 
+	int ch, zn;
+	public: 
+	
+	RNum (int a, int b) //описание конструктора с параметрами
+	{
+		ch=a;
+		zn=b;
+		cout << "Конструктор с параметрами успешно сработал!" <<"("<<this<<")"<< endl  << endl;
+	}
+	
+	~RNum () //описание деструктора
+	{
+		cout << "Деструктор успешно сработал!" <<"("<<this<<")" <<endl << endl;
+	}
+	
+	RNum (const RNum &other) //описание конктруктора копирования
+	{
+		this->ch=other.ch;
+		this->zn=other.zn;
+		cout << "Конструктор копирования успешно сработал!" <<"("<<this<<")" <<endl << endl;
+	}
+	
+	int getCh() {return ch;}; //ввод числителя с клавиатуры
+	
+	int getZn() {return zn;}; //ввод знаменателя с клавиатуры
+	
+	void setCh(int a) //установка числителя
+	{
+		ch=a;
+	}
+	
+	void setZn(int a) //установка знаменателя
+	{
+		zn=a;
+	}
+	
+	void Print() //функция для вывода числителя и знаменателя на экран
+	{
+		cout <<"Числитель равен "<<ch<<endl<<"Знаменатель равен "<<zn<<endl;
+	}
+	
+	void Scan() //функция ввода числителя и знаменателя 
+	{
+		int chisl, znam;
+		
+		cout<<"Введите числитель: ";
+		cin>>chisl;
+		setCh(chisl);
+		
+		cout<<"Введите знаменатель: ";
+		cin>>znam;
+		while (znam == 0)
+		{
+			cout<<"Ошибка. Знаменатель не может равняться нулю!"<<endl;
+			cout<<"Введите новый знаменатель: ";
+			cin>>znam;
+		}
+		
+		setZn(znam);
+		
+		Print ();
+	}
+	//описание перегрузок
+	operator float() {return ch/zn;} //перегрузка приведения к типу float
+	
+	RNum operator * (const RNum &other) //перегрузка умножения двух дробей
+	{
+		this->ch*=other.ch;
+		this->zn*=other.zn;
+		return *this;
+		
+	}
+	RNum operator * (int a) //перегрузка умножения дроби и целого числа
+	{
+		this->ch*=a;
+		return *this;
+	}
+	
+	bool operator == (const RNum &other) //перегрузка сравнения на равенство двух дробей
+	{
+		return (this->ch/this->zn)==(other.ch/other.zn);
+	 } 
+	bool operator == (int a) //перегрузка сравнения на равенство дроби и целого числа
+	
+	{
+		return (this->ch/this->zn)==a;
+	}
+	
+};
+
+float PrintFloat(float a) {return a;}
+
+int main ()
+{
+	setlocale (LC_ALL, "Russian");
+	int menu;
+	int c;
+	//организация меню
+		do {
+		system ("CLS");
+		cout <<"\tМЕНЮ:"<<endl;
+		cout << "1-Ввод данных с клавиатуры" << endl;
+		cout << "2-Вывод данных на экран" << endl;
+		cout << "3-Вывод числителя" << endl;
+		cout << "4-Вывод знаменателя" << endl;
+		cout << "5-Установка числителя" << endl;
+		cout << "6-Установка знаменателя" << endl;
+		cout << "7-Копирование" << endl;
+		cout << "8-Приведение к типу float" << endl;
+		cout << "9-Операция умножения двух рациональных чисел" << endl; 
+		cout << "10-Операция умножения рационального и целого чисел" << endl;
+		cout << "11-Сравнение на равенство двух рациональных чисел" << endl;
+		cout << "12-Сравнение на равенство рационального и целого чисел" << endl;
+		cout << "13-Выход"<<endl<<endl;
+		cout << "Выберите пункт меню: " << endl; 
+		cin >> menu;
+		
+		RNum a(1,2), b(3,4);
+		switch (menu)
+		{
+			case 1: a.Scan(); break;
+			case 2: a.Print(); break;
+			case 3: cout<<"Числитель: "<<a.getCh()<<endl; break;
+			case 4: cout<<"Знаменатель: "<<a.getZn()<<endl; break;
+			case 5: a.setCh(20); cout<<"Числитель: "<<a.getCh()<<endl; break;
+			case 6: a.setZn(10); cout<<"Знаменатель: "<<a.getZn()<<endl; break;
+			case 7: {RNum b(a); b.Print();
+				break;
+			}
+			case 8: cout<<PrintFloat(a)<<endl; break;
+			case 9:cout<<a*b<<endl; break;
+			case 10: cout<<"Введите целое число: "<<endl;
+			cin>>c;
+			cout<<a*c<<endl; break;
+			case 11: if (a==b) cout<<"Числа равны"<<endl; 
+			else cout<<"Числа не равны"; break;
+			case 12:
+			cout <<"Введите целое число: "<<endl;
+			cin>>c;
+			if (a==c) cout<<"Числа равны"<<endl;
+			else cout<<"Числа не равны"<<endl; break;	
+		
+		}
+		getchar();
+   		getchar();	
+	} while (menu!=13);
+	
+	
+	return 0;
+}
+
+
+
+
